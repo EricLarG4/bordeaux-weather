900604ab1d77332aee385d1aaed779e164e9b73f
Eric Largy <e-deuteron@protonmail.com>
2025-05-03 20:22:03 +0200

    Update Retrieve_GHCN_USW00014839.R
    Switch to Bdx station
    Removed conversion to imperial units

--- R/Retrieve_GHCN_USW00014839.R
+++ R/Retrieve_GHCN_USW00014839.R
@@ -11,7 +11,7 @@ library(tidyr)
 
 # obtain the updated file
 #   readr::read_csv downloads and unzips .csv.gz
-ghcn <- data.table::fread("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/USW00014839.csv.gz",
+ghcn <- data.table::fread("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/FR000007510.csv.gz",
                           col.names = c("id", "yearmoda", "element", "value",
                                         "mflag", "qflag", "sflag", "obs_time"),
                           colClasses = list(character=c(1:3,5:8),
@@ -25,15 +25,15 @@ ghcn.wide <- ghcn |>
   separate(col = yearmoda, sep = c(4,6), into = c("year", "month", "day")) |>
   pivot_wider(names_from = element, values_from = value) |>
   # convert from tenths of mm to inches
-  mutate(PRCP = PRCP * 0.00393701,
-         SNOW = SNOW * 0.00393701,
-         SNWD = SNWD * 0.00393701) |>
+  # mutate(PRCP = PRCP * 0.00393701,
+  #        SNOW = SNOW * 0.00393701,
+  #        SNWD = SNWD * 0.00393701) |>
   # convert from tenths of degrees C to F
-  mutate(TMAX = ((TMAX / 10) * (9/5)) + 32,
-         TMIN = ((TMIN / 10) * (9/5)) + 32) |>
+  # mutate(TMAX = ((TMAX / 10) * (9/5)) + 32,
+  #        TMIN = ((TMIN / 10) * (9/5)) + 32) |>
   mutate(date = as.Date(paste(year, month, day, sep = "-")),
          day_of_year = lubridate::yday(date)) |>
   select(year, month, day, date, day_of_year, PRCP, SNOW, SNWD,
          TMAX, TMIN)
 
-write_csv(ghcn.wide, "data/GHCN_USW00014839.csv")
+write_csv(ghcn.wide, "data/GHCN_FR000007510.csv")
